47fd95529527c5b3280a659439dbd8722182cabd
Sandeep Kumar <166711715+sandeepchiramdasu@users.noreply.github.com>
2025-02-13 12:41:27 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import cartoon from '../assets/cartoon.jpg';
+import { HiDocumentDownload } from "react-icons/hi";
+import resumeurl from "../assets/sandeepats2.pdf"
 
 const About = () => {
   return (
@@ -16,6 +18,17 @@ const About = () => {
               experiences tailored to client goals and focused on quality and impact.
             </p>
           </div>
+
+          {/*Resume section */}
+          <div className="flex gap-6 mb-6">
+            <a
+                href={resumeurl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-gray-900 text-white py-2 px-2 rounded-md flex items-center gap-2 hover:bg-gray-900 hover:scale-110 transition-transform duration-300">
+                <HiDocumentDownload size={20} className="text-cyan-500"/>Resume
+            </a>
+            </div>
 
           {/* Image Section */}
           <div className="mt-6 lg:mt-0 lg:w-1/3 flex justify-center">
